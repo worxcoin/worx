129be9417bb842dc20fb8a36577b132c8f50cd71
WG91 <43027401+WG91@users.noreply.github.com>
2019-05-30 11:39:49 +0200

    No longer accept peers below version 1.5.0.1

--- src/version.h
+++ src/version.h
@@ -12,7 +12,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70720;
+static const int PROTOCOL_VERSION = 70721;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 210;
@@ -21,9 +21,9 @@ static const int INIT_PROTO_VERSION = 210;
 static const int GETHEADERS_VERSION = 70077;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70719;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70719;
-static const int MIN_PEER_PROTO_VERSION_AFTER_BLOCK_201601 = 70719;
+static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70720;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70720;
+static const int MIN_PEER_PROTO_VERSION_AFTER_BLOCK_201601 = 70720;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
